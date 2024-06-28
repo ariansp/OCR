a76b789b9e7250299dae3df398cd3cb7d169ec1e
ariansp <ariansp08@gmail.com>
2024-06-28 16:03:19 +0700

    Add new req

--- OCR.py
+++ OCR.py
@@ -25,11 +25,14 @@ def extract_details_from_pdf(pdffile):
                 match_amount1 = re.search(r'(\d{1,3}(?:\.\d{3})*\.\d{1,3})', text)
                 specific_amount1 = match_amount1.group(1).replace('.', '.') if match_amount1 else "Amount not found"
 
+                facture_num = re.search(r'nomor faktur pajak : (.*) tanggal', text)
+                facture_num_data = facture_num.group(1).replace('.', '.') if facture_num else "Amount not found"
+
                 match_doc_ref = re.search(r'b\.7 dokumen referensi : nomor dokumen (\w+)', text)
-                specific_doc_ref = match_doc_ref.group(1).strip() if match_doc_ref else "Pattern not found for B.7 Dokumen Referensi : Nomor Dokumen"
+                specific_doc_ref = match_doc_ref.group(1).strip() if match_doc_ref else "Data not found"
 
                 pph = re.search(r'(\d{1,3}(?:\.\d{3})\.\d{1,3}) (\d)(?:\.\d{2})? (\d{1,3}(?:\.\d{3})*\.\d{1,3})', text)
-
+                tarif_tinggi = 0
                 if pph:
                     match_amount2 = pph
                     specific_amount2 = match_amount2.group(3)
@@ -37,6 +40,7 @@ def extract_details_from_pdf(pdffile):
                     match_amount2 = re.search(r'(\d{1,3}(?:\.\d{3})\.\d{2}) (\d) (\d)(?:\.\d{2})? (\d{1,3}(?:\.\d{3})*\.\d{1,3})', text)
                     if match_amount2:
                         specific_amount2 = match_amount2.group(4)
+                        tarif_tinggi = match_amount2.group(2)
                     else:
                         specific_amount2 = "Amount not found"
 
@@ -57,22 +61,107 @@ def extract_details_from_pdf(pdffile):
                     else:
                         invoice_date = "Date not found"
 
-                match_date_2 = re.search(r'nama dokumen invoice tanggal (\d\s\d) dd (\d\s\d) mm (\d\s\d\s\d\s\d)', text)
+                match_date_2 = re.search(r'nama dokumen (.*) tanggal (\d\s\d) dd (\d\s\d) mm (\d\s\d\s\d\s\d)', text)
+                nama_dokumen = ''
                 if match_date_2:
                     match_date_0 = match_date_2
-                    day = match_date_0.group(1).replace(" ", "")
-                    month = match_date_0.group(2).replace(" ", "")
-                    year = match_date_0.group(3).replace(" ", "")
+                    nama_dokumen = match_date_0.group(1).replace(" ", "")
+                    day = match_date_0.group(2).replace(" ", "")
+                    month = match_date_0.group(3).replace(" ", "")
+                    year = match_date_0.group(4).replace(" ", "")
                     invoice_date_2 = f"{year}-{month}-{day}"
+                    
                 else:
-                    match_date_0 = re.search(r'nama dokumen invoice tanggal (\d\s\d) (\d\s\d) (\d\s\d\s\d\s\d)', text)
+                    match_date_0 = re.search(r'nama dokumen (.*) tanggal (\d\s\d) (\d\s\d) (\d\s\d\s\d\s\d)', text)
                     if match_date_0:
+                        day = match_date_0.group(2).replace(" ", "")
+                        month = match_date_0.group(3).replace(" ", "")
+                        year = match_date_0.group(4).replace(" ", "")
+                        invoice_date_2 = f"{year}-{month}-{day}"
+                        nama_dokumen = match_date_0.group(1).replace(" ", "")
+                    else:
+                        invoice_date_2 = "Date not found"
+                
+                if not nama_dokumen:
+                    nama_dokumen = 'Data not found'
+                else: 
+                    nama_dokumen = nama_dokumen 
+
+                if invoice_date_2 == "Date not found":
+                    match_date_2 = re.search(r'nomor : tanggal (\d\s\d) dd (\d\s\d) mm (\d\s\d\s\d\s\d)', text)
+                    if match_date_2:
+                        match_date_0 = match_date_2
                         day = match_date_0.group(1).replace(" ", "")
                         month = match_date_0.group(2).replace(" ", "")
                         year = match_date_0.group(3).replace(" ", "")
                         invoice_date_2 = f"{year}-{month}-{day}"
                     else:
-                        invoice_date_2 = "Date not found"
+                        match_date_0 = re.search(r'nomor : tanggal (\d\s\d) (\d\s\d) (\d\s\d\s\d\s\d)', text)
+                        if match_date_0:
+                            day = match_date_0.group(1).replace(" ", "")
+                            month = match_date_0.group(2).replace(" ", "")
+                            year = match_date_0.group(3).replace(" ", "")
+                            invoice_date_2 = f"{year}-{month}-{day}"
+                        else:
+                            invoice_date_2 = "Date not found1"
+                if invoice_date_2 == "Date not found1":
+                    match_date_2 = re.search(re.escape(facture_num_data) + r' tanggal (\d\s\d) dd (\d\s\d) mm (\d\s\d\s\d\s\d)', text)
+                    if match_date_2:
+                        match_date_0 = match_date_2
+                        day = match_date_0.group(1).replace(" ", "")
+                        month = match_date_0.group(2).replace(" ", "")
+                        year = match_date_0.group(3).replace(" ", "")
+                        invoice_date_2 = f"{year}-{month}-{day}"
+                    else:
+                        match_date_0 = re.search(re.escape(facture_num_data) + r' tanggal (\d\s\d) (\d\s\d) (\d\s\d\s\d\s\d)', text)
+                        if match_date_0:
+                            day = match_date_0.group(1).replace(" ", "")
+                            month = match_date_0.group(2).replace(" ", "")
+                            year = match_date_0.group(3).replace(" ", "")
+                            invoice_date_2 = f"{year}-{month}-{day}"
+                        else:
+                            invoice_date_2 = "Date not found2"
+
+                stat_pembetulan1 = re.search(r'direktorat jenderal pajak h\.2 (.*) pembetulan ke- (.*) h\.3 pembatalan h\.5 (.*) pph tidak final', text)
+                status_pembatalan = ''
+                nilai_pembetulan = 0
+                status_pph_tidakfinal = ''
+                if stat_pembetulan1:
+                    stat_pembetulan = stat_pembetulan1
+                    status_pembetulan = stat_pembetulan.group(1).replace(' ','')
+                    nilai_pembetulan = stat_pembetulan.group(2).replace(' ','')
+                    status_pph_tidakfinal = stat_pembetulan.group(3).replace(' ','')
+                    # st.write(status_pembetulan,nilai_pembetulan,status_pph_tidakfinal)
+                else:
+                    stat_pembetulan = re.search(r'direktorat jenderal pajak h\.2 (.*) pembetulan ke- (.*) h\.3 (.*) pembatalan h\.5 (.*) pph tidak final', text)
+                    status_pembetulan = stat_pembetulan.group(1).replace(' ','')
+                    nilai_pembetulan = stat_pembetulan.group(2).replace(' ','')
+                    status_pembatalan = stat_pembetulan.group(3).replace(' ','')
+                    status_pph_tidakfinal = stat_pembetulan.group(4).replace(' ','')
+
+                if status_pembetulan == 'x' or status_pembetulan == 'X':
+                    status_pembetulan = 'Yes'
+                else: status_pembetulan = 'No'
+
+                if status_pembatalan == 'x' or status_pembatalan == 'X':
+                    status_pembatalan = 'Yes'
+                else: status_pembatalan = 'No'
+
+                if status_pph_tidakfinal == 'x' or status_pph_tidakfinal == 'X':
+                    status_pph_tidakfinal = 'Yes'
+                else: status_pph_tidakfinal = 'No'
+
+                pph_final1 = re.search(r'h\.4 pph final', text)
+                status_pph = 'No'
+                if pph_final1:
+                    pph_final = pph_final1
+                    status_pph = pph_final
+                else:
+                    pph_final = re.search(r'h\.4 (.*) pph final',text)
+                    status_pph = pph_final.group(1).replace(' ','')
+                if status_pph == 'x' or status_pph == 'X':
+                    status_pph = 'Yes'
+                else: status_pph = 'No'
 
                 extracted_details.append({
                     "H.1 NOMOR text": specific_text_h1,
@@ -83,6 +172,14 @@ def extract_details_from_pdf(pdffile):
                     "Date": invoice_date,
                     "Tanggal Dokumen": invoice_date_2,
                     "Nomor Dokumen" : specific_doc_ref,
+                    "Nama Dokumen" : nama_dokumen,
+                    "Nomor Faktur Pajak" : facture_num_data,
+                    "Status Pembetulan" : status_pembetulan,
+                    "Nilai Pembetulan" : nilai_pembetulan,
+                    'Status PPH Final' : status_pph,
+                    "Status PPH Tidak Final" : status_pph_tidakfinal,
+                    "Status Pembatalan" : status_pembatalan,
+                    "Tarif Tanpa NPWP" : tarif_tinggi,
                     "PDF_Name" : pdffile.name
                 })
             else:
@@ -94,7 +191,15 @@ def extract_details_from_pdf(pdffile):
                     "Specific amount2": "Error! Please Check The File",
                     "Date": "Error! Please Check The File",
                     "Tanggal Dokumen": "Error! Please Check The File",
+                    "Nomor Faktur Pajak" : "Error! Please Check The File",
                     "Nomor Dokumen": "Error! Please Check The File",
+                    "Nama Dokumen" : "Error! Please Check The File",
+                    "Tarif Tanpa NPWP": "Error! Please Check The File",
+                    "Status Pembetulan" : "Error! Please Check The File",
+                    "Nilai Pembetulan" : "Error! Please Check The File",
+                    "Status PPH Final" : "Error! Please Check The File",
+                    "Status PPH Tidak Final" : "Error! Please Check The File",
+                    "Status Pembatalan" : "Error! Please Check The File",
                     "PDF_Name" : pdffile.name
                 })
 
@@ -133,11 +238,19 @@ if uploaded_files:
         "Date": "Tanggal",
         "PDF_Name" : "PDF_Name",
         "Tanggal Dokumen": "Tanggal_Dokumen",
-        "Nomor Dokumen": "Nomor_Dokumen"
+        "Nomor Dokumen": "Nomor_Dokumen",
+        "Nama Dokumen": "Nama_Dokumen",
+        "Status Pembetulan" : "Status_Pembetulan",
+        "Nilai Pembetulan" : "Nilai_Pembetulan",
+        'Status PPH Final' : 'Status_PPH_Final',
+        "Status PPH Tidak Final" : "Status_PPH_Tidak_Final",
+        "Status Pembatalan" : "Status_Pembatalan",
+        "Tarif Tanpa NPWP": "Tarif_Tanpa_NPWP",
+        "Nomor Faktur Pajak": "Nomor_Faktur_Pajak"
     }, inplace=True)
 
     # Reordering columns to match the desired format
-    df = df[['NO', 'PDF_Name', 'Nama_Pemotong', 'NPWP_Pemotong', 'Pasal', 'Pph', 'Nilai_Obj_Pemotongan', 'Pph_potput', 'Nomor_Bukti', 'Tanggal', 'Tanggal_Dokumen','Nomor_Dokumen', 'Alamat', 'NTPN']]
+    df = df[['NO', 'PDF_Name', 'Nama_Pemotong', 'NPWP_Pemotong', 'Pasal', 'Pph', 'Status_Pembetulan', 'Nilai_Pembetulan', 'Status_PPH_Final', 'Status_PPH_Tidak_Final', 'Status_Pembatalan', 'Nilai_Obj_Pemotongan', 'Pph_potput', 'Tarif_Tanpa_NPWP', 'Nomor_Faktur_Pajak', 'Nomor_Bukti', 'Tanggal', 'Tanggal_Dokumen','Nomor_Dokumen','Nama_Dokumen', 'Alamat', 'NTPN']]
 
     # Convert date columns to datetime format
     df['Tanggal'] = pd.to_datetime(df['Tanggal'], format='%Y-%m-%d', errors='coerce').dt.strftime('%Y-%m-%d')
